ffa3bc2f0d15a7152770610286662cc1d505adfe
Nitheesh <qxnitheesh@gmail.com>
2024-12-31 13:50:22 +0530

    Make the update button interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -37,9 +37,13 @@ cart.forEach((cartItem) => {
                       cartItem.quantity
                     }</span>
                   </span>
-                  <span class="update-quantity-link link-primary">
+                  <span class="update-quantity-link link-primary js-update-link" data-product-id="${
+                    matchingProduct.id
+                  }">
                     Update
                   </span>
+                  <input class="quantity-input">
+                  <span class="save-quantity-link link-primary">Save</span>
                   <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
                     matchingProduct.id
                   }">
@@ -118,3 +122,14 @@ function updateCartQuantity() {
 }
 
 updateCartQuantity();
+
+document.querySelectorAll(".js-update-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    console.log(productId);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.classList.add("is-editing-quantity");
+  });
+});
